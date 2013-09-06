0b42859400b7621240574b33301b60a1289bc5f4
MakerSquare Student <students@themakersquare.com>
2013-09-06 09:53:13 -0500

    Initial Commit! It has begun...

---
+++ library.rb
@@ -0,0 +1,29 @@
+module Library
+# Library module will be ableto keep track of books and their information.
+#  -It will allow users to borrow AND return books.
+#
+#  -Users may add books of their own.
+#
+#  -It will keep track of which books they checked out.
+#
+#  -Users are limited to 2 books.
+#
+#  -If a user has one overdue book, they will not be able to borrow another.
+#   until they have returned the first book.
+# 
+#  -Users can pull status reports showing them a list of books
+#   and their status
+# 
+#  -Users may pull a status report on the books they have checked out
+#   and how many days are left.
+# 
+#  -Users may pull the status of a specific book of their choosing.
+# 
+#  -Kaoru Kohashigawa > dev@kaoruk.com
+# 
+
+
+
+
+
+end
