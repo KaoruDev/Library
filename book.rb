00af50a7dc326f4e8696c99b037164d943198144
MakerSquare Student <students@themakersquare.com>
2013-09-06 10:12:46 -0500

    Define Book class initialize

--- book.rb
+++ book.rb
@@ -1,6 +1,29 @@
 class Book
-#class Book in book.rb will hold book's information such as
-# author, title, description, number of copies, ratings,
+# class Book in book.rb will hold book's information such as
+# author, title, description, number of copies, ratings, reviews,
 # year published and edition.
 
+  attr_reader :title, :author, :desc, :year, :edition
+
+
+  attr_accessor :num_copies, :rating, :reviews
+
+# Runs whenever user adds a new book to Library.
+# Assigns and storees attributes
+#
+# title - Book title = String
+# author - Book title = String
+# desc - Book title = String
+# num_copies - How many copies of a book = FixNum
+# year - year published = FixNum
+# edition - what edition is the book? = FixNum
+
+def initialize(title, author, desc, num_copies, year, edition)
+  @title = title
+  @author = author
+  @desc = desc
+  @year = year
+  @num_copies = num_copies
+  @year = year
+  @edition = edition
 end
