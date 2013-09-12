4686fdee0859e6945138c9cc4aa22129268ba995
MakerSquare Student <students@themakersquare.com>
2013-09-12 07:59:25 -0500

    Add method to schedule future check outs

--- library.rb
+++ library.rb
@@ -1,35 +1,23 @@
+# Library module will be able to keep track of books and users.
+#
+# Library.methods verify_user & verify_book verifies if user and book is in the database.
+#
+# add_book creates a new book class. Library keeps track of it in the collection hash via title.
+#
+#
+
+
 module Library
-  # Library module will be ableto keep track of books and their information.
-  #  -It will allow users to borrow AND return books.
-  #
-  #  -Users may add books of their own.
-  #
-  #  -It will keep track of which books they checked out.
-  #
-  #  -Users are limited to 2 books.
-  #
-  #  -If a user has one overdue book, they will not be able to borrow another.
-  #   until they have returned the first book.
-  # 
-  #  -Users can pull status reports showing them a list of books
-  #   and their status
-  # 
-  #  -Users may pull a status report on the books they have checked out
-  #   and how many days are left.
-  # 
-  #  -Users may pull the status of a specific book of their choosing.
-  # 
-  #  -Kaoru Kohashigawa > dev@kaoruk.com
+
 
   @collection = {} #Keeps track of all books
   @users = {} #Keeps track of all users
 
-  require './book.rb'
   # Brings in Book class which will hold book's information such as
   # author, title, description, number of copies, ratings, reviews,
   # year published and edition.
+  require './book.rb'
 
-  require './user.rb'
   # Brings in User class which will hold information on each user, such as:
   #
   # - Name
@@ -37,13 +25,13 @@ module Library
   # - Array of borrowed books.
   # - Overdue books
   # - Time borrowed, time due.
+  require './user.rb'
 
   # csv to add CSV functionality
   require 'csv'
 
   # Verifies if user and book exist in database. Returns nil if they don't
-
-  def verify_user(username)
+  def self.verify_user(username)
     if @users != {}
       @users.each_key{ |user_key|
       if user_key == username.to_sym
@@ -55,7 +43,7 @@ module Library
     end
   end
 
-  def verify_book(book_title)
+  def self.verify_book(book_title)
     if @collection != {}
       @collection.each_key{ |book_key|
       if book_key == book_title.to_sym
@@ -67,23 +55,22 @@ module Library
     end
   end
 
-
+  # Creates a new book passing arguments to the Book class for storage.
+  # Book class then Assigns and stores attributes
+  #
+  # title: Book title = String
+  # author: Book title = String
+  # desc: Book title = String
+  # num_copies: How many copies of a book = FixNum
+  # year: year published = FixNum
+  # edition: what edition is the book? = FixNum
+  #
+  # Then it is added to the Library's collection.
   def self.add_book(title, author, desc, year, edition, num_copies = 1)
-    # Creates a new book passing arguments to the Book class for storage.
-    # Book class then Assigns and stores attributes
-    #
-    # title: Book title = String
-    # author: Book title = String
-    # desc: Book title = String
-    # num_copies: How many copies of a book = FixNum
-    # year: year published = FixNum
-    # edition: what edition is the book? = FixNum
-    #
-    # Then it is added to the Library's collection.
 
+    # If a user tries to add a book already in a collection, 
+    # dup_copy will simply increase the number of available books.
     if @collection != {} && @collection[title.to_sym]
-      # If a user tries to add a book already in a collection, 
-      # dup_copy will simply increase the number of available books.
       @collection[title.to_sym].dup_copy
     else
       book = Book.new(title, author, desc, year, edition, num_copies)
@@ -91,13 +78,13 @@ module Library
     end
   end
 
+  # Creates a new user to hold information:
+  #
+  # - username: used to id user.
+  # - pin_num: used to verify user
+  # - answer: answer to security question incase user loses pin
+  # NOTE: Secruity Question has not been created, since there is no output in program yet.
   def self.add_user(username, pin_num, answer)
-    # Creates a new user to hold information:
-    #
-    # - username: used to id user.
-    # - pin_num: used to verify user
-    # - answer: answer to security question incase user loses pin
-    # NOTE: Secruity Question has not been created, since there is no output in program yet.
     
     if @users != {} && @users[username.to_sym]
       puts "Sorry, #{username} is taken."
@@ -108,10 +95,9 @@ module Library
     end
   end
 
-
+  # Prints a list of books, how many copies there are, how many copies are checked out,
+  # who they are checked out by, and how many are available.  
   def self.list
-    # Prints a list of books, how many copies there are, how many copies are checked out,
-    # who they are checked out by, and how many are available.
 
     @collection.each { |name, book|
       print %<
@@ -123,9 +109,9 @@ module Library
     }
   end
 
+  # Prints out more information on a specific book.
+  # Title, Author, Description, Year published, Edition, How many avaiable.
   def self.info_on(book_title)
-    # Prints out more information on a specific book.
-    # Title, Author, Description, Year published, Edition, How many avaiable.
     look_up = @collection[book_title.to_sym]
     print %<
       Title:              #{look_up.title}
@@ -137,8 +123,8 @@ module Library
      >
   end
 
+  # prints out of a list of books that you have checked out.
   def self.my_books(user)
-    # prints out of a list of books that you have checked out.
     puts %<
     Here is a list of books you currently have checked_out>
 
@@ -157,7 +143,6 @@ module Library
   # verifies user (string) and pin (interger)
 
   def self.check_out(book_title, username, pin)
-
     # Verifies if the book is in @collection, if user has an account and has entered the correct pin, the book is available,
     # the user has no overdue books or has reach the borrowing limit. If true, sends information to both book and user class
     # to update changes. 
@@ -175,8 +160,6 @@ module Library
           current_user = @users[username.to_sym]
         end
       }
-    else
-      puts "Either there is no such book, or you are not registered. Please try again." 
     end
 
     if current_book && current_user && current_book.num_in >=1 && !current_user.overdue && !current_user.max_borrowed && current_user.pin_num == pin
@@ -201,7 +184,6 @@ module Library
   # returns book to library.
   # user is converted to a symbol to access hash key in @users to determine who user is.
   def self.return(book_title, user, pin)
-    
     # Verifies if user has a this specific book checked out.
     # Verifies user's pin number. If both are true,
     # user's record is cleared, and book count is returned to collection.
@@ -280,26 +262,44 @@ module Library
   # book_title: String - Used to pull book object in @collection array
   # username: String - Used to store who is scheduling a future check out.
   def self.future_check_out(book_title, username)
+    current_user, current_book = nil
+
     if @collection != {} && @users != {}
-
-
+      @collection.each_key{ |book_key|
+        if book_key == book_title.to_sym
+          current_book = @collection[book_key]
+        end
+      }
+      @users.each_key{ |user_key|
+        if user_key == username.to_sym
+          current_user = @users[username.to_sym]
+        end
+      }
     end
 
 
-
-    current_book = @collection[book_title.to_sym]
-    if current_book.num_in == 0 && current_book.future_check_out.empty?
+    if current_book && current_user && current_book.num_in == 0 && current_book.future_check_out.empty? && current_user.overdue && current_user.max_borrowed
       current_book.borrowed_by.each {|x|
-        puts x.class
+        due_date = x.time_table[current_book.title.to_sym]
       }
 
-      current_book.schedule_future_check_out(@users[username.to_sym], due_date)
+      current_book.schedule_future_check_out(@users[username.to_sym], due_date) if current_user && current_book
+    end
+      
+    if !current_book
+      puts "No such book exists in our collection."
+    elsif current_book.num_in > 0
+      puts "You may not schedule a future check because there are copies of the book available now."
+    elsif !current_user
+      puts "Please register before borrowing any books."
+    elsif !current_user.overdue
+      puts "You may not schedule a future check out because you have an overdue book."
+    elsif !current_user.max_borrowed
+      puts "You may not schedule a future check out because you are at your borrowing limit."
+    else
       puts "Got it, we will notify you when #{book_title} is ready for you!"
     end
   end
-
-  # Enables users to extends borrow time.
-
 end
 
 
@@ -328,7 +328,3 @@ Library.write_review("LOTR", 5, "1st Review for LOTR")
 Library.write_review("LOTR", 2, "2nd Review for LOTR")
 
 Library.read_review("LOTR")
-
-
-
-
